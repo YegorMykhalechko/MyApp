27a296b9e8cb5259e8f68a18f7354df7d5e76609
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2023-04-25 22:30:10 +0200

    day4 hm

--- MyApp/MyConsoleApp/Program.cs
+++ MyApp/MyConsoleApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool isWoman = true;
+int age = 33;
+
+if (isWoman)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyna");
+}
+else { 
+    Console.WriteLine("Mężczyna");
+}
