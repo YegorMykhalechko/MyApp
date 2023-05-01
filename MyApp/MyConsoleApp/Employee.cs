7d24db92e1d127406b517ca239a12744931efa16
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2023-05-01 15:48:19 +0200

    hm6

---
+++ MyApp/MyConsoleApp/Employee.cs
@@ -0,0 +1,30 @@
+namespace MyConsoleApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+
+        public Employee(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public int Result
+        {
+            get { return this.score.Sum(); }
+        }
+
+        public void AddScore(int score)
+        {
+            this.score.Add(score);
+        }
+    }
+}
--- MyApp/MyConsoleApp/Program.cs
+++ MyApp/MyConsoleApp/Program.cs
@@ -1,16 +1,44 @@
-﻿int number = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
+﻿using MyConsoleApp;
 
-for (int i = 0; i < 10; i++)
+Employee employee1 = new Employee("name1", "surname1", 21);
+Employee employee2 = new Employee("name2", "surname2", 22);
+Employee employee3 = new Employee("name3", "surname3", 23);
+
+employee1.AddScore(3);
+employee1.AddScore(6);
+employee1.AddScore(8);
+employee1.AddScore(3);
+employee1.AddScore(1);
+
+employee2.AddScore(1);
+employee2.AddScore(5);
+employee2.AddScore(4);
+employee2.AddScore(5);
+employee2.AddScore(7);
+
+employee3.AddScore(9);
+employee3.AddScore(9);
+employee3.AddScore(5);
+employee3.AddScore(9);
+employee3.AddScore(9);
+
+List<Employee> employees = new List<Employee>() { employee1, employee2, employee3 };
+
+int maxScore = 0;
+Employee currentEmployee = null;
+
+foreach (Employee employee in employees)
 {
-    int counter = 0;
-    foreach (char letter in letters)
+    if (maxScore < employee.Result)
     {
-        if (int.Parse(letter.ToString()) == i)
-        {
-            counter++;
-        }
+        maxScore = employee.Result;
+        currentEmployee = employee;
     }
-    Console.WriteLine(i + "=>" + counter);
 }
+
+Console.WriteLine(
+    "First Name: " + currentEmployee.FirstName + "; " +
+    "Last Name: " + currentEmployee.LastName + "; " +
+    "Age: " + currentEmployee.Age + "; " +
+    "Score: " + currentEmployee.Result
+);
